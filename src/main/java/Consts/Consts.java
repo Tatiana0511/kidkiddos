9256fe90a30e74b02d5dccfc75671a7355fbb4a1
Tatiana Golovei <golovei.tatiana@gmail.com>
2020-12-23 14:56:48 -0500

    Test

--- src/main/java/Consts/Consts.java
+++ src/main/java/Consts/Consts.java
@@ -4,4 +4,5 @@ public class Consts {
     public static final String MAIN_URL = "https://kidkiddos.com/";
     public static final String CONTACT_US_URL ="https://kidkiddos.com/pages/contact-us";
     public static final String LOGIN_URL ="https://kidkiddos.com/account/login";
+    public static final String LOG_URL ="https://kidkiddos.com/account/login";
 }
